0b37cc12e89bed0bfe9e43c9cec4451595f5268f
Nazar Levkovych <nazar.levkovich.2018@gmail.com>
2024-02-07 14:11:48 +0200

    initial

---
+++ src/components/Login.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState({});
+
+  const handlerClick = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users/1"
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setUser(data);
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className="container">
+      <span className="user">{user.name}</span>
+      <form>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button disabled={!username || !password} onClick={handlerClick}>
+          {loading ? "please wait" : "Login"}
+        </button>
+        <span
+          data-testid="error"
+          style={{ visibility: error ? "visible" : "hidden" }}
+        >
+          Something went wrong!
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
